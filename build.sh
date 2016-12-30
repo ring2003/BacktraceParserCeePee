05529fb338dfa1344ca79817ad7a2f1d0281d7b1
ring2003 <441345965@qq.com>
2016-12-30 01:31:38 -0800

    Fix bug in build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sed -i 's/PKG_CHECK_MODULES.*//g' configure
 ./configure CFLAGS="-I/grid/common/pkgsData/glib-v2.48.0/Linux/RHEL5.0-2010-x86_64/lib/glib-2.0/include -I/grid/common/pkgsData/glib-v2.48.0/Linux/CentOS6.0-2015-x86_64/include/glib-2.0 -L/include -L/grid/common/pkgsData/glib-v2.48.0/Linux/CentOS6.0-2015-x86_64/lib -Wl,-rpath -Wl,/grid/common/pkgsData/glib-v2.48.0/Linux/CentOS6.0-2015-x86_64/lib"
 make
 cd ..
-ln -s 3rd/lib/.lib/
+ln -s 3rd/lib/.libs/libbtparser.so .
 mkdir build
 cd build
 cmake ..
